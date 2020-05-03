ff97837f78edca7dc14a1ed23f9a35969abe809d
rida300 <rida490@gmail.com>
2020-05-03 13:28:23 -0500

    Add project files.

---
+++ RazorAppForArduino/Pages/CreateGraph.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorAppForArduino;
+using static RazorAppForArduino.InvoiceModel;
+
+namespace RazorAppForArduino.Pages
+{
+    public class CreateGraph : PageModel
+    {
+        private readonly InvoiceService _invoiceService;
+
+        public List<InvoiceModel> InvoiceList;
+        public CreateGraph(InvoiceService invoiceService)
+        {
+            _invoiceService = invoiceService;
+        }
+        public void OnGet()
+        {
+            InvoiceList = _invoiceService.GetInvoices();
+        }
+
+        public JsonResult OnGetInvoiceChartData()
+        {
+           
+            InvoiceList = _invoiceService.GetInvoices();
+            var invoiceChart = new CategoryChartModel();
+            invoiceChart.AmountList = new List<double>();
+            invoiceChart.CategoryList = new List<string>();
+
+            foreach (var inv in InvoiceList)
+            {
+                invoiceChart.AmountList.Add(inv.Amount);
+                invoiceChart.CategoryList.Add(inv.CostCategory);
+            }
+
+            return new JsonResult(invoiceChart);
+        }
+
+    }
+}
---
+++ RazorAppForArduino/Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.IO.Ports;
+using System.Web;
+
+namespace RazorAppForArduino.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; set; }
+
+        private SerialPort myport;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+           
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = "Rida's Razor Page!";
+        }
+
+        public void OnPostLedOn()
+        {
+            myport = new SerialPort
+            {
+                BaudRate = 9600,
+                PortName = "COM11"
+            };
+            myport.Open();
+
+            myport.Write("1");
+            myport.Close();
+            
+        }
+
+        public void OnPostLedOff()
+        {
+            myport = new SerialPort
+            {
+                BaudRate = 9600,
+                PortName = "COM11"
+            };
+            myport.Open();
+
+            myport.Write("2");
+            myport.Close();
+        }
+
+    }
+}
