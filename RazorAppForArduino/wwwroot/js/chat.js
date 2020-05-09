5118fabe15958883d4828730405e2786eeeb4cae
rida300 <rida490@gmail.com>
2020-05-09 18:23:32 -0500

    before tokenizing attempt

--- RazorAppForArduino/wwwroot/js/chat.js
+++ RazorAppForArduino/wwwroot/js/chat.js
@@ -2,26 +2,16 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/myHub").build();
 
-//Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
-
 connection.on("ReceiveMessage", function (message) {
-    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    //var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     var li = document.createElement("li");
-    li.textContent = msg;
+    li.textContent = message;
     document.getElementById("messagesList").appendChild(li);
 });
 
 connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
+
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
-document.getElementById("sendButton").addEventListener("click", function (event) {
-    var message = document.getElementById("messageInput").value;
-    connection.invoke("SendMessage", message).catch(function (err) {
-        return console.error(err.toString());
-    });
-    event.preventDefault();
-});
